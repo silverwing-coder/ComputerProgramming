ba171dee4544c9ef795eb5396d07fd2399a7cf34
Sangmork "SAM" Park <sangmork.park@gmail.com>
2023-04-03 09:39:27 -0400

    UPDATE

--- CS111$Programming-2(C++)/ClassCodes/Week5/T03.cpp
+++ CS111$Programming-2(C++)/ClassCodes/Week5/T03.cpp
@@ -14,7 +14,8 @@ int main()
     int x = 5;
     int y = 10;
     cout << "Before swap: x= " << x << ", y= " << y << endl;
-    swap(&x, &y);
+    // swap(&x, &y);
+    swap(x, y);
     cout << "After swap: x = " << x << ", y = " << y << endl;
     return 0;
 }
